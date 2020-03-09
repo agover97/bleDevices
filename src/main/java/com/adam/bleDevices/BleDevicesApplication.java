3428e5f7b7550d5d801d38fda4985820624d7938
agover97 <adamgover97@gmail.com>
2020-03-09 14:39:26 -0400

    initial commit

---
+++ src/main/java/com/adam/bleDevices/BleDevicesApplication.java
@@ -0,0 +1,13 @@
+package com.adam.bleDevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BleDevicesApplication.class, args);
+	}
+
+}
